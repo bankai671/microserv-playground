b8716b6f82e9a1d2afb7f6e3a6b42bda028f5fc5
John Doe <bankai671@proton.me>
2024-08-06 03:13:46 +0200

    init server, and add / route

--- auth-service/src/main.rs
+++ auth-service/src/main.rs
@@ -1,3 +1,23 @@
-fn main() {
-    println!("Hello, world!");
+use axum::{
+    routing::get,
+    Router
+};
+
+use tracing::info;
+
+#[tokio::main]
+async fn main() {
+    tracing_subscriber::fmt::init();
+    let num: usize = num_cpus::get();
+
+    let app = Router::new()
+        .route("/", get(root));
+
+    let listner = tokio::net::TcpListener::bind("0.0.0.0:3001").await.unwrap();
+    info!("Server is running on port 3001!, with {num} cpus");
+    axum::serve(listner, app).await.unwrap();
+}
+
+async fn root () -> &'static str {
+    "hello from / endpoint\n"
 }
