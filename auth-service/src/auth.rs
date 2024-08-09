2b821ef38dfc4fc33b97fa9dbc5ea0648849daa0
John Doe <bankai671@proton.me>
2024-08-09 06:40:42 +0200

    refactor and add login method prototype

--- auth-service/src/auth.rs
+++ auth-service/src/auth.rs
@@ -2,67 +2,44 @@ use axum::{
     Json,
     http::StatusCode
 };
-use argon2::{self, Config};
-use rand::Rng;
-use serde::{Serialize, Deserialize};
-use std::sync::LazyLock;
-
-#[derive(Deserialize, Debug)]
-pub struct RegisterPayload {
-    email: String,
-    username: String,
-    password: String,
-    confirm_password: String
-}
-
-#[derive(Serialize, Debug)]
-pub struct RegisterPayloadReturn {
-    uid: u32,
-    email: String,
-    username: String,
-    password: String
-}
-
-#[derive(Serialize, Debug)]
-pub struct ErrorMessage {
-    message: String
-}
-
-static ARGON2_CFG: LazyLock<Config> = LazyLock::new(|| Config {
-    variant: argon2::Variant::Argon2id,
-    version: argon2::Version::Version13,
-    mem_cost: 32768,
-    time_cost: 1,
-    lanes: 8,
-    ..Default::default()
-});
+use crate::utils::{self};
+use crate::model::{RegisterDto, LoginDto, LoginResponse, ErrorMessage};
 
 pub async fn register (
-Json(payload): Json<RegisterPayload>
-) -> Result<(StatusCode, Json<RegisterPayloadReturn>), (StatusCode, Json<ErrorMessage>)> {
+    Json(payload): Json<RegisterDto>
+)-> StatusCode
+{
     if payload.password != payload.confirm_password {
-        let err_msg = ErrorMessage {
-            message: "password not match with confirm_password".to_string()
-        };
-
-        return Err((StatusCode::UNAUTHORIZED, Json(err_msg)));
+        return StatusCode::UNAUTHORIZED;
     };
     
     // TODO:
     // check if user exist in user-service
-    // if exist return 409 and ErrorMessage
+    // if exist return 409
+    // let hashed_password = utils::hash_password(&payload.password).unwrap();
+    // call user-service set user into db with hashed_password
+ 
+    StatusCode::OK
+}
 
-    let salt: [u8; 16] = rand::thread_rng().gen();
+pub async fn login (
+    Json(_payload): Json<LoginDto>
+) -> Result<(StatusCode, Json<LoginResponse>), (StatusCode, Json<ErrorMessage>)> {
+    // TODO:
+    // check if user is already logged in (store user from user-service in variable user to use it later)
+    // compare password and hash
+    // in future add email verefication
 
-    
-    let encrypted = argon2::hash_encoded(payload.password.as_bytes(), &salt, &ARGON2_CFG).unwrap();
-    
-    let res_json = RegisterPayloadReturn {
-                    uid: 1,
-                    email: payload.email.to_string(),
-                    username: payload.username.to_string(),
-                    password: encrypted
+    let access_token = utils::generate_access_token().await;
+    let refresh_token = utils::generate_refresh_token().await;
+
+    // save token to redis - (key) token - (value) user_id
+
+    let response_json = LoginResponse {
+        access_token: access_token.to_string(),
+        refresh_token: refresh_token.to_string()
     };
 
-    Ok((StatusCode::OK, Json(res_json)))
+    Ok((StatusCode::OK, Json(response_json)))
 }
+
