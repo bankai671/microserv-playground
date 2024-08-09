3f3959845bd011543e987b436bec78c838667328
John Doe <bankai671@proton.me>
2024-08-09 21:27:56 +0200

    update port in curl

--- auth-service/register.sh
+++ auth-service/register.sh
@@ -1,5 +1,5 @@
 curl -X POST \
     -v \
     -H "Content-Type: application/json" \
-    --url localhost:3001/register \
+    --url localhost:8001/register \
     -d '{"email":"test@gmail.com","username":"test","password":"password","confirm_password":"password"}'
