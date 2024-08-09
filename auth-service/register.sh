31a71084937ac3051f8416b9c997c1aee382275e
John Doe <bankai671@proton.me>
2024-08-09 06:41:12 +0200

    add -v flag for more info

--- auth-service/register.sh
+++ auth-service/register.sh
@@ -1,4 +1,5 @@
 curl -X POST \
+    -v \
     -H "Content-Type: application/json" \
     --url localhost:3001/register \
     -d '{"email":"test@gmail.com","username":"test","password":"password","confirm_password":"password"}'
