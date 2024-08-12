1bed06f6d855ece296c67f3db441a38f1d6a0e4a
John Doe <bankai671@proton.me>
2024-08-12 22:22:10 +0200

    deleted old sqlc files

--- user-service/query.sql
+++
@@ -1,6 +0,0 @@
--- query.sql
-
--- name: CreateUser :one
-INSERT INTO users (email, username, password)
-VALUES ($1, $2, $3)
-RETURNING id, email, username, password, created_at;
--- user-service/schema.sql
+++
@@ -1,8 +0,0 @@
--- schema.sql
-CREATE TABLE IF NOT EXISTS users (
-    id SERIAL PRIMARY KEY,
-    email VARCHAR(255) NOT NULL UNIQUE,
-    username VARCHAR(50) NOT NULL UNIQUE,
-    password TEXT NOT NULL,
-    created_at TIMESTAMPTZ DEFAULT now()
-);
