e75006b7ef1ec6c1f430afd6181c765638400ff7
John Doe <bankai671@proton.me>
2024-08-13 02:17:53 +0200

    refactor app entrypoint

--- user-service/main.go
+++ user-service/main.go
@@ -1,43 +1,23 @@
 package main
 
 import (
-	"context"
 	"log"
-	"github.com/bankai671/microserv-playground/user-service/config"
-	"github.com/bankai671/microserv-playground/user-service/internal/db"
+
 	"github.com/bankai671/microserv-playground/user-service/router"
-	"github.com/jackc/pgx/v5"
+    "github.com/bankai671/microserv-playground/user-service/database"
 
 	"github.com/gofiber/fiber/v3"
+    "github.com/gofiber/fiber/v3/middleware/logger"
 )
 
-func main() {
-    ctx := context.Background()
+func init() {
+    database.ConnectDB()
+}
 
+func main() {
     app := fiber.New()
     
-    log.Printf("env url: %s", config.Config("DB_URL"))
-
-    conn, err := pgx.Connect(ctx, config.Config("DB_URL"))
-    if err != nil {
-        log.Fatal(err)
-    }
-
-    defer conn.Close(ctx)
-    
-    queries := user.New(conn)
-    
-    createdUser, err := queries.CreateUser(ctx, user.CreateUserParams {
-        Email: "pizdec",
-        Username: "test",
-        Password: "pohui",
-    })
-
-    log.Print(createdUser)
-
-    if err != nil {
-        log.Fatal(err)
-    }
+    app.Use(logger.New())
 
     router.SetupRoutes(app)
 
