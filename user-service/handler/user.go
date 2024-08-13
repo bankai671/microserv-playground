888dad2e77a8f482af75d2083db06c925adeee44
John Doe <bankai671@proton.me>
2024-08-13 02:17:24 +0200

    update CreateUser function

--- user-service/handler/user.go
+++ user-service/handler/user.go
@@ -1,11 +1,39 @@
 package handler
 
 import (
-    "github.com/gofiber/fiber/v3"
+	"log"
+
+	"github.com/bankai671/microserv-playground/user-service/database"
+	"github.com/bankai671/microserv-playground/user-service/database/sqlc"
+	"github.com/gofiber/fiber/v3"
 )
 
-func CreateUser(c fiber.Ctx) error {
-    //ctx := c.Context()
-
-    return c.SendString("POST: hello from user-service /user endpoint")
+type CreateUserRequest struct {
+    Email       string  `json:"email"`
+    Username    string  `json:"username"`
+    Password    string  `json:"password"`
 }
+
+func CreateUser(c fiber.Ctx) error {
+    ctx := c.Context()
+    
+    user := CreateUserRequest {}
+
+    if err := c.Bind().JSON(&user); err != nil {
+        return err
+    }
+
+    createdUser, err := database.Queries.CreateUser(ctx, sqlc.CreateUserParams {
+        Email: user.Email,
+        Username: user.Username,
+        Password: user.Password,
+    })
+
+    if err != nil {
+        log.Println(err)
+        return c.Status(fiber.StatusConflict).SendString("User already exist!")
+    }
+    
+    return c.Status(fiber.StatusOK).JSON(createdUser)
+}
+
