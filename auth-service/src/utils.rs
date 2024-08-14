ff2f385dc44a0c8df777b2d194daa995d7a69107
John Doe <bankai671@proton.me>
2024-08-14 07:44:35 +0200

    update generate_refresh_token function to generate token with different expire time

--- auth-service/src/utils.rs
+++ auth-service/src/utils.rs
@@ -9,7 +9,7 @@ use crate::model::TokenClaims;
 pub fn hash_password (password: &str) -> Result<String, argon2::Error> {
     let salt: [u8; 16] = rand::thread_rng().gen();
 
-    argon2::hash_encoded(password.as_bytes(), &salt, &argon2::Config{
+    argon2::hash_encoded(password.as_bytes(), &salt, &argon2::Config {
         variant: argon2::Variant::Argon2id,
         version: argon2::Version::Version13,
         mem_cost: 32768,
@@ -42,5 +42,19 @@ pub async fn generate_access_token (uid: &str, jwt_secret: &str) -> String {
 }
 
 pub async fn generate_refresh_token (uid: &str, jwt_secret: &str) -> String {
-    generate_access_token(uid, jwt_secret).await
+    let now = chrono::Utc::now();
+    let exp = (now + chrono::Duration::hours(24)).timestamp() as usize;
+ 
+    let claims = TokenClaims {
+        sub: uid.to_string(),
+        exp
+    };
+
+    let token = jsonwebtoken::encode(
+        &Header::default(),
+        &claims,
+        &EncodingKey::from_secret(jwt_secret.as_ref())
+    ).unwrap();
+    
+    token
 }
